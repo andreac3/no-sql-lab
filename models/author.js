469ea8e307a51f650c13d023bed4d3036656ebe1
andreac3 <andrea.calderon,zuluaga@gmail.com>
2020-07-24 21:06:55 -0500

    Lab 6 - MongoDB

---
+++ models/author.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const AuthorSchema = new mongoose.Schema({
+  nombre: {
+    type: String,
+    required: true,
+  },
+  apellido:{
+    type: String,
+    require: false,
+  },
+  publicados: {
+    type: String,
+    required: true,
+  },
+  pais: {
+    type: String,
+    required: true,
+  },
+
+});
+
+AuthorSchema.set('toObject', {
+  virtuals: true,
+});
+AuthorSchema.set('toJSON', {
+  virtuals: true,
+});
+
+
+const Author = mongoose.model('Author', AuthorSchema);
+module.exports = Author;
